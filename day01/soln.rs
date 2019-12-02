91a4adf74dafa6a4bc1f095d4ff8658b186f098e
Bart Massey <bart.massey@gmail.com>
2019-12-01 16:11:37 -0800

    refactored and cleaned up day 01

--- day01/soln.rs
+++ day01/soln.rs
@@ -2,16 +2,26 @@
 // Please see the file COPYING in this distribution
 // for license terms.
 
-// Advent of Code Day 1.
-// Bart Massey 2019
+//! Advent of Code Day 1.  
+//! Bart Massey 2019
 
-fn get_fuel() -> i64 {
+/// Read the masses from stdin and return an iterator over
+/// them. We use `i64` because it is sufficiently large and
+/// the calculation in `calc_fuel_recur()` may go negative.
+fn get_masses() -> impl Iterator<Item=i64> {
     aoc::input_lines()
-        .map(|m| m.parse::<i64>().unwrap() / 3 - 2)
+        .map(|m| m.parse::<i64>().unwrap())
+}
+
+/// Calculate the total fuel consumed by the given masses.
+fn calc_total_fuel(masses: impl Iterator<Item=i64>) -> i64 {
+    masses
+        .map(|m| m / 3 - 2)
         .sum()
 }
 
-fn recur_fuel(mut cur_fuel: i64) -> i64 {
+/// Do the recurrent fuel calculation for a single mass.
+fn calc_fuel_recur(mut cur_fuel: i64) -> i64 {
     let mut tot_fuel = 0;
     loop {
         cur_fuel = cur_fuel / 3 - 2;
@@ -24,23 +34,27 @@ fn recur_fuel(mut cur_fuel: i64) -> i64 {
 }
 
 #[test]
-fn test_recur_fuel_examples() {
-    assert_eq!(2, recur_fuel(14));
-    assert_eq!(966, recur_fuel(1969));
-    assert_eq!(50346, recur_fuel(100756));
+fn test_calc_fuel_recur() {
+    assert_eq!(2, calc_fuel_recur(14));
+    assert_eq!(966, calc_fuel_recur(1969));
+    assert_eq!(50346, calc_fuel_recur(100756));
 }
 
-fn get_recur_fuel() -> i64 {
-    aoc::input_lines()
-        .map(|m| recur_fuel(m.parse::<i64>().unwrap()))
+/// Calculate the total recurrent fuel cost for the given
+/// masses.
+fn calc_total_fuel_recur(masses: impl Iterator<Item=i64>) -> i64 {
+    masses
+        .map(calc_fuel_recur)
         .sum()
 }
 
+/// Run the problem.
 pub fn main() {
     let part = aoc::get_part();
+    let masses = get_masses();
     let fuel = match part {
-        aoc::Part1 => get_fuel(),
-        aoc::Part2 => get_recur_fuel(),
+        aoc::Part1 => calc_total_fuel(masses),
+        aoc::Part2 => calc_total_fuel_recur(masses),
     };
     println!("{}", fuel);
 }
