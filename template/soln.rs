9c5098773f9c9f9a6664f35bb5e868a436d3996f
Bart Massey <bart.massey@gmail.com>
2019-12-01 16:12:01 -0800

    added rustdoc to template soln.rs

--- template/soln.rs
+++ template/soln.rs
@@ -2,8 +2,8 @@
 // Please see the file COPYING in this distribution
 // for license terms.
 
-// Advent of Code Day <day>.
-// Bart Massey 2019
+//! Advent of Code Day <day>.  
+//! Bart Massey 2019
 
 pub fn main() {
     let part = aoc::get_part();
