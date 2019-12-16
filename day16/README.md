a43aad150e6d93c39c3d107b241683f9060f2349
Bart Massey <bart.massey@gmail.com>
2019-12-15 22:35:46 -0800

    finished day 16 part 1; quit

---
+++ day16/README.md
@@ -0,0 +1,42 @@
+# Advent of Code 2019: Day 16
+Bart Massey
+
+…aaand I'm done with Advent of Code. Part 1 was
+straightforward enough, if fiddly. It took me about 1.5
+hours to generate some reasonable, clean code.
+
+When Part 2 came up, it was obvious that I couldn't just
+brute force it unless I wanted to wait at least
+overnight. It was also clear that some mildly clever modulo
+arithmetic stuff was intended, taking advantage of the
+periodicity in the patterns to make Part 2 run in about the
+same time as Part 1.
+
+I started to write that code.
+
+Then I lost the will to continue. It would have been at
+least another 1.5 hours, and I still haven't finished the
+persnickety Day 14 solution yet.
+
+If I am going to do this again, it will be because there are
+nice problems every single night that a reasonably competent
+programmer like myself can solve in less than two hours for
+both parts. I don't need a Christmas Project — I need a
+Christmas Recreation.
+
+Enough.
+
+---
+
+Solution to Part 1 of
+[this problem](https://adventofcode.com/2019/day/16).
+
+Save your problem input to `input.txt` and run with
+
+    cargo run --release 1 <input.txt
+
+---
+
+This program is licensed under the "MIT License".
+Please see the file LICENSE in this distribution
+for license terms.
---
+++ day16/soln.rs
@@ -0,0 +1,97 @@
+// This program is licensed under the "MIT License".
+// Please see the file LICENSE in this distribution
+// for license terms.
+
+//! Advent of Code Day 16.  
+//! Bart Massey 2019
+
+use std::char::from_digit;
+use std::convert::TryInto;
+use std::iter::repeat;
+use std::string::ToString;
+
+#[derive(Debug, Clone)]
+struct Digits(Vec<i64>);
+
+impl Digits {
+    fn new(input: &str) -> Self {
+        let digits = input
+            .chars()
+            .map(|c| i64::from(aoc::digit_to_int(c)))
+            .collect();
+        Self(digits)
+    }
+
+    fn make_patterns(&self) -> Vec<Self> {
+        let n = self.0.len();
+        let pat = [0, 1, 0, -1];
+        let make_pat = |i| {
+            let pi = pat
+                .iter()
+                .cloned()
+                .flat_map(|p| repeat(p).take(i + 1))
+                .cycle()
+                .skip(1)
+                .take(n)
+                .collect();
+            Self(pi)
+        };
+        (0..n).map(make_pat).collect()
+    }
+
+    fn sum(&self, phases: usize) -> Self {
+        let ninput = self.0.len();
+        let patterns = self.make_patterns();
+        let mut input = self.0.clone();
+        for _ in 0..phases {
+            let mut next = Vec::with_capacity(ninput);
+            for pat in &patterns {
+                let pat = &pat.0;
+                let digit: i64 = input
+                    .iter()
+                    .cloned()
+                    .zip(pat.iter().cloned())
+                    .map(|(q, r)| q * r)
+                    .sum();
+                next.push(digit.abs() % 10);
+            }
+            input = next;
+        }
+        Self(input)
+    }
+}
+
+impl ToString for Digits {
+    fn to_string(&self) -> String {
+        self.0
+            .iter()
+            .cloned()
+            .map(|d| {
+                from_digit(d.try_into().unwrap(), 10)
+                    .expect("non-digit")
+            })
+            .collect()
+    }
+}
+
+#[test]
+fn test_sum() {
+    let digits = Digits::new("12345678");
+    let sum = digits.sum(1);
+    assert_eq!(sum.to_string(), "48226158");
+    let sum = digits.sum(4);
+    assert_eq!(sum.to_string(), "01029498");
+
+    let digits = Digits::new("80871224585914546619083218645595");
+    let sum = digits.sum(100).to_string();
+    assert_eq!(&sum[0..8], "24176176");
+}
+
+pub fn main() {
+    let input = aoc::input_line();
+    let digits = Digits::new(&input);
+    let part = aoc::get_part();
+    assert!(part == aoc::Part1);
+    let sum = digits.sum(100).to_string();
+    println!("{}", &sum[0..8]);
+}
